f408d472dbed35984b9776c037b5e59130893a64
Dc520134 <dc_1419@163.com>
2018-09-01 10:40:49 +0800

    所有module升级到jdk8

---
+++ rpc-sample-app/src/main/java/com/kerry/rpc/sample/app/HelloServiceTest.java
@@ -0,0 +1,38 @@
+package com.kerry.rpc.sample.app;
+
+import com.kerry.rpc.client.RpcProxy;
+import com.kerry.rpc.simple.client.HelloService;
+import com.kerry.rpc.simple.client.Person;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring.xml")
+public class HelloServiceTest {
+
+	@Autowired
+	private RpcProxy rpcProxy;
+
+	@Test
+	public void helloTest1() {
+		// 调用代理的create方法，代理HelloService接口
+		HelloService helloService = rpcProxy.create(HelloService.class);
+		
+		// 调用代理的方法，执行invoke
+		String result = helloService.hello("World");
+		System.out.println("服务端返回结果：");
+		System.out.println(result);
+	}
+
+	@Test
+	public void helloTest2() {
+		HelloService helloService = rpcProxy.create(HelloService.class);
+		String result = helloService.hello(new Person("Yong", "Huang"));
+		System.out.println("服务端返回结果：");
+		System.out.println(result);
+	}
+}
